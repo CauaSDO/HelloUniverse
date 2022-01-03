df8ea38affce46c8b1b1b9fc4a54b9065893dacc
CauaSDO <cauasilva@icloud.com>
2022-01-03 10:33:06 -0300

    Initial Commit

---
+++ HelloUniverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloUniverse
+//
+//  Created by caua on 03/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
